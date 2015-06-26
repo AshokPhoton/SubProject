1cc63ad7f86e0326284df83ce717642afd55260c
Ashok Kumar <ashokkumar@IOS-App.local>
2015-06-26 16:49:02 +0530

    Committing Sub Project

---
+++ DepProject/DepProject/DepProject.h
@@ -0,0 +1,13 @@
+//
+//  DepProject.h
+//  DepProject
+//
+//  Created by Ashok Kumar on 26/06/15.
+//  Copyright (c) 2015 Ashok Kumar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DepProject : NSObject
+
+@end
---
+++ DepProject/DepProject/DepProject.m
@@ -0,0 +1,13 @@
+//
+//  DepProject.m
+//  DepProject
+//
+//  Created by Ashok Kumar on 26/06/15.
+//  Copyright (c) 2015 Ashok Kumar. All rights reserved.
+//
+
+#import "DepProject.h"
+
+@implementation DepProject
+
+@end
